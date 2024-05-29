9a3401372937c44f4c88a24e1d76f4b759abed3f
lukagugushvili <gugushvili58@gmail.com>
2024-05-29 08:44:39 -0700

    create cardsStyles file and cards styled hare

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "../styles/ContainerStyles";
-import styled from "styled-components";
-import { count } from "console";
 import { DataType } from "../types/DataTypes";
+import { Card, CardsWrapper, Details } from "../styles/CardsStyles";
 
 const Cards = () => {
   const [countries, setCountries] = useState<DataType[]>([]);
@@ -26,8 +25,6 @@ const Cards = () => {
 
     fetchData();
   }, []);
-
-  console.log(countries);
 
   return (
     <div>
@@ -65,59 +62,3 @@ const Cards = () => {
 };
 
 export default Cards;
-
-const CardsWrapper = styled.div`
-  margin: 0 1rem;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(264px, 1fr));
-  gap: 50px;
-`;
-
-const Card = styled.div`
-  cursor: pointer;
-  max-height: 350px;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  border-radius: 0px 0px 5px 5px;
-  box-shadow: 0px 0px 7px 2px #00000008;
-  background-color: #ffffff;
-
-  & img {
-    flex: 1;
-    width: 100%;
-    height: 50%;
-    border-radius: 5px 5px 0px 0px;
-  }
-
-  @media (max-width: 700px) {
-    align-items: center;
-    text-align: center;
-  }
-`;
-
-const Details = styled.div`
-  padding: 1rem;
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-
-  & h2 {
-    padding: 15px 0 10px 0;
-    font-size: 18px;
-  }
-
-  & h3 {
-    font-size: 14px;
-    font-weight: 600;
-
-    & span {
-      font-size: 14px;
-      font-weight: 300;
-      padding-left: 10px;
-    }
-  }
-`;
---
+++ src/styles/CardsStyles.tsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const CardsWrapper = styled.div`
+  margin: 0 1rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(264px, 1fr));
+  gap: 50px;
+`;
+
+export const Card = styled.div`
+  cursor: pointer;
+  max-height: 350px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  border-radius: 0px 0px 5px 5px;
+  box-shadow: 0px 0px 7px 2px #00000008;
+  background-color: #ffffff;
+
+  & img {
+    flex: 1;
+    width: 100%;
+    height: 50%;
+    border-radius: 5px 5px 0px 0px;
+  }
+
+  @media (max-width: 700px) {
+    align-items: center;
+    text-align: center;
+  }
+`;
+
+export const Details = styled.div`
+  padding: 1rem;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  & h2 {
+    padding: 15px 0 10px 0;
+    font-size: 18px;
+  }
+
+  & h3 {
+    font-size: 14px;
+    font-weight: 600;
+
+    & span {
+      font-size: 14px;
+      font-weight: 300;
+      padding-left: 10px;
+    }
+  }
+`;
